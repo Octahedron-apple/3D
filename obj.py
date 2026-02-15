e215ad8a6f1ed7d46a3d2bb4598bd8e2a62eb510
Octahedron-apple <bhavya1singh1@gmail.com>
2026-02-15 20:52:12 +0530

    Refactor camera class constructor and render method
    Updated camera class constructor to include Focal_Length parameter and modified render method to accept a camera instance.

--- obj.py
+++ obj.py
@@ -1,8 +1,11 @@
 import numpy as np
 class camera():
-    def __init__(X,Y,Z,X_rotate,Y_rotate,Z_rotate)
+    def __init__(X: float,Y: float,Z: float,X_Rotate: float,Y_Rotate: float,Z_Rotate: float,Focal_Length: float):
+        self.position=np.array([X,Y,Z])
+
 class point():
     def __init__(X: float=0,Y: float=0,Z: float=0):
         self.main=np.array([X,Y,Z])
         self.projection=np.array([0,0])
-    def render():
+    def render(camera: camera):
+
