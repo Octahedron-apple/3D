47d0312882e70d1bde6fe1fcf8504142080ade9b
Octahedron-apple <bhavya1singh1@gmail.com>
2026-02-15 20:43:04 +0530

    Add camera and point classes in obj.py

---
+++ obj.py
@@ -0,0 +1,8 @@
+import numpy as np
+class camera():
+    def __init__(X,Y,Z,X_rotate,Y_rotate,Z_rotate)
+class point():
+    def __init__(X: float=0,Y: float=0,Z: float=0):
+        self.main=np.array([X,Y,Z])
+        self.projection=np.array([0,0])
+    def render():
